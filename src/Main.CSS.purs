9b78ae0cd7e426b21aa535d7ec79d5af4733406f
Pavel Shapov <pavel.shapov@metalamp.io>
2022-06-10 06:04:19 +0700

    Remove unexisting import

--- src/Main.CSS.purs
+++ src/Main.CSS.purs
@@ -4,7 +4,6 @@ import Prelude
 
 import CSS ((?))
 import CSS as CSS
-import CSS.Color (blue)
 import CSS.Common (auto)
 import CSS.Geometry (margin)
 
@@ -14,5 +13,4 @@ main = do
     CSS.width $ CSS.pct 100.0
     CSS.maxWidth $ CSS.px 1200.0
     CSS.minHeight $ CSS.vh 100.0
-    CSS.backgroundColor blue
     margin (CSS.px 0.0) auto (CSS.px 0.0) auto
