1d4b68ca06e5bd8f7a9160dfec34f9ac84cc6669
Pavel Shapov <pavel.shapov@metalamp.io>
2022-06-10 23:08:07 +0700

    Test styles in Component.Header

--- src/Component/Header/HTML.purs
+++ src/Component/Header/HTML.purs
@@ -1,10 +1,10 @@
 module Component.Header.HTML where
 
-import Prelude
-
+import Component.Header.Styles (styles)
 import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
 
 header :: forall a b. HH.HTML a b
 header = HH.header
-  []
+  [ HP.classes [ styles.header ] ]
   []
---
+++ src/Component/Header/Styles.purs
@@ -0,0 +1,11 @@
+module Component.Header.Styles
+  ( styles
+  )
+  where
+
+import Halogen.HTML as HH
+
+foreign import styles ::
+  { header :: HH.ClassName
+  , link :: HH.ClassName 
+  }
