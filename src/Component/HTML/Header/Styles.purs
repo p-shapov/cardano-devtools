03c9480173529b126f4a3c9c6fa5dd2eefcd2c97
Pavel Shapov <pavel.shapov@metalamp.io>
2022-06-28 03:49:00 +0700

    Update styles for header

--- src/Component/HTML/Header/Styles.purs
+++ src/Component/HTML/Header/Styles.purs
@@ -1,11 +1,9 @@
-module Component.HTML.Header.Styles
-  ( styles
-  ) where
+module Component.HTML.Header.Styles where
 
 foreign import styles
-  :: { header :: String
-     , logo :: String
-     , nav :: String
-     , link :: String
+  :: { "header" :: String
+     , "logo" :: String
+     , "nav" :: String
+     , "link" :: String
      , "link--active" :: String
      }
